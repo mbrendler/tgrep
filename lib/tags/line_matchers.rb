124a877399087634b34c191d8a8e05afbcfe4e4a
Maik Brendler <maik.brendler@injixo.com>
2017-07-16 10:48:29 +0200

    Add missing matcher classes

---
+++ lib/tags/line_matchers.rb
@@ -0,0 +1,12 @@
+
+TagNameCaseSensitiveCompare = Struct.new(:search) do
+  def match?(line)
+    line.start_with?(search)
+  end
+end
+
+TagNameCaseInSensitiveCompare = Struct.new(:search) do
+  def match?(line)
+    line.downcase.start_with?(search)
+  end
+end
