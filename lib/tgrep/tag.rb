850464375dfbb50655ca8d0eac5eb947a9fbe3ea
Maik Brendler <maik.brendler@injixo.com>
2017-07-29 17:01:05 +0200

    Order methods of Tag class

--- lib/tgrep/tag.rb
+++ lib/tgrep/tag.rb
@@ -14,6 +14,22 @@ class Tag
     define_method(name){ @data[name] }
   end
 
+  def class_name
+    self.class.class_name(@data)
+  end
+
+  def absolute_filename
+    File.join(@base_dir, filename)
+  end
+
+  def pattern
+    @pattern ||= data[:pattern].dup.tap do |p|
+      p.slice!(0) if start_pattern?
+      p.slice!(-1) if end_pattern?
+      p.gsub!('\\/', '/')
+    end
+  end
+
   def match?(line)
     return line == pattern if start_pattern? && end_pattern?
     return line.start_with?(pattern) if start_pattern?
@@ -21,37 +37,13 @@ class Tag
     line.include?(pattern)
   end
 
-  def start_pattern?
-    @start_pattern ||= data[:pattern][1] == '^'
-  end
-
-  def end_pattern?
-    @end_pattern ||= data[:pattern][-1] == '$'
-  end
-
-  def pattern
-    @pattern ||= data[:pattern][1..-1].tap do |p|
-      p.slice!(0) if p[0] == '^'
-      p.slice!(-1) if p[-1] == '$'
-      p.gsub!('\\/', '/')
-    end
-  end
-
   def identifier
     return "#{name}#{simple_signature}" if class_name && name.start_with?(class_name)
     "#{class_name}::#{name}#{simple_signature}"
   end
 
-  def class_name
-    self.class.class_name(@data)
-  end
-
   def add_line_number(nr)
     (@line_numbers ||= []) << nr
-  end
-
-  def absolute_filename
-    File.join(@base_dir, filename)
   end
 
   def line_numbers
@@ -64,10 +56,6 @@ class Tag
     kind_cmp = kind_index - other.kind_index
     return kind_cmp unless kind_cmp.zero?
     (line_numbers[0] || 0).to_i - (other.line_numbers[0] || 0).to_i
-  end
-
-  def kind_index
-    KIND_ORDER.index(kind) || -1
   end
 
   def code
@@ -107,26 +95,49 @@ class Tag
     end
   end
 
-  def self.parse(line)
-    base, extra = line.split('/;"')
-    name, filename, pattern = base.split("\t", 3)
-    _, kind, *rest = extra.chomp.split("\t")
-    data = {name: name, filename: filename, pattern: pattern, kind: kind}
-    add_extra(data, rest)
+  private
+
+  def start_pattern?
+    @start_pattern ||= data[:pattern][0] == '^'
   end
 
-  def self.add_extra(data, extra_strs)
-    extra_strs.each do |x|
-      k, v = x.split(':', 2)
-      data[k.to_sym] = v
+  def end_pattern?
+    @end_pattern ||= data[:pattern][-1] == '$'
+  end
+
+  def kind_index
+    KIND_ORDER.index(kind) || -1
+  end
+
+  class << self
+    def parse(line)
+      base, extra = line.split('/;"')
+      name, filename, pattern = base.split("\t", 3)
+      _, kind, *rest = extra.chomp.split("\t")
+      data = {
+        name: name,
+        filename: filename,
+        pattern: pattern[1..-1],
+        kind: kind
+      }
+      add_extra(data, rest)
     end
-    data
-  end
 
-  def self.class_name(data)
-    data[:class_name] ||= begin
-      class_name = data[:class] || data[:enum] || data[:typeref] || (data[:kind] == 'c' ? data[:name] : nil)
-      data[:namespace] ? "#{data[:namespace]}::#{class_name}" : class_name
+    def class_name(data)
+      data[:class_name] ||= begin
+        class_name = data[:class] || data[:enum] || data[:typeref] || (data[:kind] == 'c' ? data[:name] : nil)
+        data[:namespace] ? "#{data[:namespace]}::#{class_name}" : class_name
+      end
+    end
+
+    private
+
+    def add_extra(data, extra_strs)
+      extra_strs.each do |x|
+        k, v = x.split(':', 2)
+        data[k.to_sym] = v
+      end
+      data
     end
   end
 end
