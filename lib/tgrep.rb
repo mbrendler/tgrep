f1820a9896678aa3bf3f70770eda1ff465bda4da
Maik Brendler <maik.brendler@injixo.com>
2017-07-31 19:40:43 +0200

    Add module to pretty print tags

--- lib/tgrep.rb
+++ lib/tgrep.rb
@@ -1,3 +1,4 @@
 require_relative 'tgrep/config'
 require_relative 'tgrep/tag'
 require_relative 'tgrep/tags'
+require_relative 'tgrep/pretty'
---
+++ lib/tgrep/pretty.rb
@@ -0,0 +1,44 @@
+module Tgrep
+  module Pretty
+    if $stdout.tty?
+      TPUT_PRIMARY = "#{`tput setaf 2`}#{`tput bold`}".freeze
+      TPUT_KEY = `tput setaf 4`.freeze
+      TPUT_CLEAR = "#{`tput op`}#{`tput sgr0`}".freeze
+    else
+      TPUT_PRIMARY = ''.freeze
+      TPUT_KEY = ''.freeze
+      TPUT_CLEAR = ''.freeze
+    end
+
+    def self.print(tags, config)
+      tag = tags[0]
+      puts("#{pretty_class(tag)}#{pretty_name(tag)}#{best_signature(tags)} // #{tag.code}")
+      tags.each do |t|
+        filename = config.full_path ? t.absolute_filename : t.filename
+        line_numbers = t.line_numbers.empty? ? [0] : t.line_numbers
+        line_numbers.each do |line_number|
+          puts(" #{TPUT_KEY}#{t.kind}#{TPUT_CLEAR} #{filename}:#{line_number}")
+        end
+      end
+      puts
+    end
+
+    def self.best_signature(tags)
+      tags.map(&:signature).max_by(&:size)
+    end
+
+    def self.local_name(tag)
+      tag.name.sub(/^#{tag.data[:class]}::/, '')
+    end
+
+    def self.pretty_class(tag)
+      class_name = tag.class_name
+      class_name.empty? ? '' : "#{TPUT_KEY}#{class_name}#{TPUT_CLEAR}::"
+    end
+
+    def self.pretty_name(tag)
+      name = tag.name.sub(/^#{tag.data[:class]}::/, '')
+      "#{TPUT_PRIMARY}#{name}#{TPUT_CLEAR}"
+    end
+  end
+end
