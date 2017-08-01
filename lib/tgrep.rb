7813d5f7a9944f59c1a48e0593e0ccdbdd2b097c
Maik Brendler <maik.brendler@injixo.com>
2017-08-01 16:37:27 +0200

    Refactor main function

--- lib/tgrep.rb
+++ lib/tgrep.rb
@@ -2,3 +2,22 @@ require_relative 'tgrep/config'
 require_relative 'tgrep/tag'
 require_relative 'tgrep/tags'
 require_relative 'tgrep/pretty'
+require_relative 'tgrep/line_handlers'
+
+module Tgrep
+  def self.main(args)
+    config = Config.parse(args)
+    tags = parse_tagfile(config)
+    exit(1) if tags.empty?
+    tags.collect_line_numbers(config.encoding)
+    tags.sort!
+    tags.each{ |tag| Pretty.print(tag, config) }
+  end
+
+  def self.parse_tagfile(config)
+    tags = Tags.new
+    handler = LineHandlers.line_handler(config, tags)
+    config.open_tagfile.each_line(&handler)
+    tags
+  end
+end
---
+++ lib/tgrep/line_handlers.rb
@@ -0,0 +1,37 @@
+module Tgrep
+  module LineHandlers
+    def self.line_handler(config, tags)
+      if config.outline
+        outline_line_handler(config, tags)
+      else
+        default_line_handler(config, tags)
+      end
+    end
+
+    def self.outline_line_handler(config, tags)
+      proc do |line|
+        next if line.start_with?('!_TAG_')
+        hash = Tag.parse(line)
+        next if Tag.full_class_name(hash) != config.tag
+        tags.add(Tag.new(hash, config.base_dir))
+      end
+    end
+
+    def self.default_line_handler(config, tags)
+      matcher = config.matcher
+      proc do |line|
+        next if line.start_with?('!_TAG_')
+        next unless matcher.match?(line)
+        hash = Tag.parse(line)
+        next unless check_patterns(hash[:filename], config.file_patterns)
+        next unless check_patterns(Tag.full_class_name(hash), config.classes)
+        tags.add(Tag.new(hash, config.base_dir))
+      end
+    end
+
+    def self.check_patterns(str, patterns)
+      return true if patterns.empty?
+      patterns.any?{ |pattern| /#{pattern}/.match?(str) }
+    end
+  end
+end
